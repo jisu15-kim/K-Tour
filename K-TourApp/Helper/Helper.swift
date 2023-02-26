20bc2590c13170c900ffc7b181a15311948f6a38
jisu15-kim <jisu15.kim@gmail.com>
2023-02-27 03:54:23 +0900

    fix: network ignore

--- K-TourApp/Helper/Helper.swift
+++ K-TourApp/Helper/Helper.swift
@@ -19,7 +19,7 @@ enum contentTypeID: Int {
 }
 
 struct Helper {
-    private let key = "O%2FG920ZjfGIFYshoBYqghwh3hF22e6g9KOcQj6T2D1eAw6LqO18gKbSGOTmmvhyaVPkiQmnh3qQfhMNvU3A4YQ%3D%3D"
+    private let key = Secret.apiKey
     private let url = ""
     private let listViewDomain = "areaBasedList?"
     private let commonDataDomain = "detailCommon?"
---
+++ K-TourApp/Secret/Secret.swift
@@ -0,0 +1,12 @@
+//
+//  Secret.swift
+//  K-TourApp
+//
+//  Created by 김지수 on 2023/02/27.
+//
+
+import Foundation
+
+struct Secret {
+    static let apiKey = "O%2FG920ZjfGIFYshoBYqghwh3hF22e6g9KOcQj6T2D1eAw6LqO18gKbSGOTmmvhyaVPkiQmnh3qQfhMNvU3A4YQ%3D%3D"
+}
